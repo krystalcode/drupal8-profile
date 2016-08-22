7682c24daffa48e723078fa527c6d9d691af4d5c
neerajsingh <neerajsingh@3202261.no-reply.drupal.org>
2016-08-22 07:52:47 -0500

    Issue #2686839 by rakesh.gectcr, neerajsingh, mglaman: @file is not required for classes, interfaces and traits

--- src/Plugin/EntityReferenceSelection/ProfileSelection.php
+++ src/Plugin/EntityReferenceSelection/ProfileSelection.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\profile\Plugin\EntityReferenceSelection\TermSelection.
- */
-
 namespace Drupal\profile\Plugin\EntityReferenceSelection;
 
 use Drupal\Core\Entity\Plugin\EntityReferenceSelection\DefaultSelection;
