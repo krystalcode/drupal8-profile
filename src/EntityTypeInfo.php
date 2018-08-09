8e8426cc30e3ef50097311895eebf6698db23c6d
Shabana Navas <snavas@acromedia.com>
2018-08-09 12:19:15 +0530

    Cleanup from code review.

--- src/EntityTypeInfo.php
+++ src/EntityTypeInfo.php
@@ -70,47 +70,89 @@ class EntityTypeInfo implements ContainerInjectionInterface {
    * @see hook_entity_operation()
    */
   public function entityOperation(EntityInterface $entity) {
+    $operations = $this->publishedOperations($entity);
+    $operations += $this->unpublishedOperations($entity);
+
+    return $operations;
+  }
+
+  /**
+   * Adds published operations for the profile entity.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The profile entity.
+   *
+   * @return array
+   *   An array of operations.
+   */
+  protected function publishedOperations(EntityInterface $entity) {
     $operations = [];
 
-    /** @var \Drupal\profile\Entity\ProfileInterface $entity */
-
-    $profile_type_storage = $this->entityTypeManager->getStorage('profile_type');
-    /** @var \Drupal\profile\Entity\ProfileTypeInterface $profile_type */
-    $profile_type = $profile_type_storage->load($entity->bundle());
+    if (!$entity->isActive()) {
+      return $operations;
+    }
 
     // If the profile is published, add an unpublish operation.
-    if ($entity->isActive()) {
-      if (!$entity->isDefault()) {
-        $operations['set_default'] = [
-          'title' => $this->t('Mark as default'),
-          'url' => $entity->toUrl('set-default'),
-          'parameter' => $entity,
-          'weight' => 50,
-        ];
-      }
-
-      // Display an unpublish button.
-      if ($entity->access('unpublish')) {
-        $operations['unpublish'] = [
-          'title' => $profile_type->getUnpublishLabel(),
-          'url' => Url::fromRoute('entity.profile.unpublish', [
-            'profile' => $entity->id()
-          ]),
-          'weight' => 51,
-        ];
-      }
+    if (!$entity->isDefault()) {
+      $operations['set_default'] = [
+        'title' => $this->t('Mark as default'),
+        'url' => $entity->toUrl('set-default'),
+        'parameter' => $entity,
+        'weight' => 50,
+      ];
     }
-    else {
-      if ($entity->access('publish')) {
-        // Display a publish button.
-        $operations['publish'] = [
-          'title' => $profile_type->getPublishLabel(),
-          'url' => Url::fromRoute('entity.profile.publish', [
-            'profile' => $entity->id()
-          ]),
-          'weight' => 51,
-        ];
-      }
+
+    // Display an unpublish button.
+    if ($entity->access('unpublish')) {
+      /** @var \Drupal\profile\Entity\ProfileInterface $entity */
+
+      $profile_type_storage = $this->entityTypeManager->getStorage('profile_type');
+      /** @var \Drupal\profile\Entity\ProfileTypeInterface $profile_type */
+      $profile_type = $profile_type_storage->load($entity->bundle());
+
+      $operations['unpublish'] = [
+        'title' => $profile_type->getUnpublishLabel(),
+        'url' => Url::fromRoute('entity.profile.unpublish', [
+          'profile' => $entity->id(),
+        ]),
+        'weight' => 51,
+      ];
+    }
+
+    return $operations;
+  }
+
+  /**
+   * Adds unpublished operations for the profile entity.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The profile entity.
+   *
+   * @return array
+   *   An array of operations.
+   */
+  protected function unpublishedOperations(EntityInterface $entity) {
+    $operations = [];
+
+    if ($entity->isActive()) {
+      return $operations;
+    }
+
+    if ($entity->access('publish')) {
+      /** @var \Drupal\profile\Entity\ProfileInterface $entity */
+
+      $profile_type_storage = $this->entityTypeManager->getStorage('profile_type');
+      /** @var \Drupal\profile\Entity\ProfileTypeInterface $profile_type */
+      $profile_type = $profile_type_storage->load($entity->bundle());
+
+      // Display a publish button.
+      $operations['publish'] = [
+        'title' => $profile_type->getPublishLabel(),
+        'url' => Url::fromRoute('entity.profile.publish', [
+          'profile' => $entity->id(),
+        ]),
+        'weight' => 51,
+      ];
     }
 
     return $operations;
